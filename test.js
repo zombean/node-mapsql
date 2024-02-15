99372cc2781910beb0b2314e55c2421f2cc28345
frogbean <49106938+frogbean@users.noreply.github.com>
2024-02-15 21:15:33 +0000

    added test

---
+++ test.js
@@ -0,0 +1,9 @@
+const { load } = require("./mapsql.js");
+console.log("loading map 'test'");
+const map = load("test");
+console.log("assigning world to hello");
+map.set("hello", "world");
+const value = map.get("hello");
+console.log("hello value:", value);
+if(value === "world") console.log("test succesful!");
+else console.warn("something went wrong!");
